3b6364b8667d1f66b2a539f8bcd23643253c2837
Philipp Eschenbach <philipp@uberall.com>
2020-07-09 17:20:12 +0200

    remove useless annotation

--- data-jdbc/src/main/java/com/uberall/DistributedLock.java
+++ data-jdbc/src/main/java/com/uberall/DistributedLock.java
@@ -1,13 +1,12 @@
 package com.uberall;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(
-        uniqueConstraints = @UniqueConstraint(columnNames = {"name"})
-)
 public class DistributedLock {
 
     @NotNull
